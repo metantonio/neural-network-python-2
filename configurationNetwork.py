993584554b1699bd9a86c629f020da295f785c8e
Antonio Martínez <antonio_martinez88@hotmail.com>
2021-09-23 22:40:10 -0400

    updated

--- configurationNetwork.py
+++ configurationNetwork.py
@@ -1,12 +1,12 @@
 #layers can be [input, hidden, hidden,...,hidden,...,output]
-neurons_in_layers=[3,8,8,1]
+neurons_in_layers=[2,8,1]
 
 class TrainingExample():
     def __init__(self, inputs, output):
         self.inputs = inputs
         self.output = output
 
-#Example of training data
+#Example of training dataset with inputs [a,b,c] and , output/target
 examples = [TrainingExample([0, 0, 1], 0),
                 TrainingExample([0, 1, 1], 1),
                 TrainingExample([1, 0, 1], 1),
